54fcf56ca528088863bd6ceaea73f65b3a50d049
rmholsen <jaguar.kitty@gmail.com>
2021-06-07 16:46:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+/* let doubledAndSummed = [1, 2, 3].reduce( (total, element) => element * 2 + total, 0)
+
+function declaration = array.reduce, i.e. invoking the reduce function
+total, element = the total at each point as it iterates over the array, and each array element
+element * 8, multiply the element by 2, + total, and add it to the total
+
+returns 12
+
+without the zero at the end it returns 11, because the first element isn't used in the element function?? something like that. brain no worky
+*/
+
+// const totalBatteries = (batches) => {
+//     let result = batches.reduce((total, element) => element + total, 0)
+//     return result 
+// }
+// NO look up dingus
+
+const totalBatteries = batteryBatches.reduce( (total, element) => element + total, 0);
